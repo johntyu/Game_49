b30eea9aeb50d324bbf48f835341b7c3403fe087
Zach Ahn <zach.ahn@gmail.com>
2013-11-17 04:32:05 -0500

    special attacks

---
+++ Assets/Scripts/ManagerSpecialAttackManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ManagerSpecialAttackManager : MonoBehaviour {
+
+	private UnitManager unitManager;
+
+	// Use this for initialization
+	void Start () {
+		unitManager = GameObject.FindGameObjectWithTag("PlayerUnitManager").GetComponent<UnitManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		bool q = Input.GetKeyDown(KeyCode.Q);
+		bool w = Input.GetKeyDown(KeyCode.W);
+		bool e = Input.GetKeyDown(KeyCode.E);
+		bool r = Input.GetKeyDown(KeyCode.R);
+
+		foreach(GameObject unit in unitManager.GetSelectedUnits()){
+			if (q) {
+				unit.SendMessage("SpecialAttack_Q", SendMessageOptions.DontRequireReceiver);
+			}
+			if (w) {
+				unit.SendMessage("SpecialAttack_W", SendMessageOptions.DontRequireReceiver);
+			}
+			if (e) {
+				unit.SendMessage("SpecialAttack_E", SendMessageOptions.DontRequireReceiver);
+			}
+			if (r) {
+				unit.SendMessage("SpecialAttack_R", SendMessageOptions.DontRequireReceiver);
+			}
+		}
+	}
+}
