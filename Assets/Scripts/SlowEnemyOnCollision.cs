b1bf97fa4ff4da8f2975b9b48675dcd78f7f83b7
barykade <barykade@umich.edu>
2013-11-19 18:11:46 -0500

    Platform Object

--- Assets/Scripts/SlowEnemyOnCollision.cs
+++ Assets/Scripts/SlowEnemyOnCollision.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 public class SlowEnemyOnCollision : MonoBehaviour {
 	
-	public float radius = 1.2f;
+	public float radius = 3.2f;
 	public float bulletDamage = 1.0f;
 	public LayerMask mask;
 	public float aoeRadius;
@@ -15,7 +15,7 @@ public class SlowEnemyOnCollision : MonoBehaviour {
 	float curTime = 0.0f;
 	
 	void Start () {
-		radius = 0.2f;
+		radius = 3.2f;
 	    destroyTime = 6.0f;
 		curTime = 0.0f;
 	}
